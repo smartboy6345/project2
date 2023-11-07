3f65d41d97ba00c3b2a796b3c99cd00326f525c7
smartboy6345 <duddujeevankumar@gmail.com>
2023-11-07 22:19:45 +0530

    first commit

---
+++ app2/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def jampandu(request):
+    return HttpResponse('<h1><marquee>This  is one of the biggest task to create the application cant remember the syntax </h1></marquee>')
+def jigailu_rani(request):
+    return HttpResponse('<h1><marquee>To Remember the all the sytax in codding, mind is playing  jigilala with student ')
